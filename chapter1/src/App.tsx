e31a32e0f385d7e548a86e31d381e86d4b21f2aa
Yu Asabe <yu.asabe@tier4.jp>
2024-02-28 13:40:45 +0900

    fix: change App.tsx
    Signed-off-by: Yu Asabe <yu.asabe@tier4.jp>

--- chapter1/src/App.tsx
+++ chapter1/src/App.tsx
@@ -5,17 +5,84 @@ function App() {
   return (
     <>
       <Box>
-        <h1>動画再生アプリ</h1>
+        <h1>LIVE</h1>
         <ReactPlayer
           url={
-            "https://d13pitkuolwx3f.cloudfront.net/output/test.m3u8"
+            "https://d3nvdnvywolj6.cloudfront.net/output/capture.m3u8"
           }
           id="MainPlay"
-          playing
-          loop
+          playing={true}
+          loop={true}
           controls={true}
-          width="1280px"
-          height="720px"
+          muted={true}
+          width="600px"
+          height="360px"
+          style={{float: 'left'}}
+        />
+        <ReactPlayer
+          url={
+            "https://d3nvdnvywolj6.cloudfront.net/output/capture (2).m3u8"
+          }
+          id="MainPlay"
+          playing={true}
+          loop={true}
+          controls={true}
+          muted={true}
+          width="600px"
+          height="360px"
+          style={{float: 'left'}}
+        />
+        <ReactPlayer
+          url={
+            "https://d3nvdnvywolj6.cloudfront.net/output/capture (8).m3u8"
+          }
+          id="MainPlay"
+          playing={true}
+          loop={true}
+          controls={true}
+          muted={true}
+          width="600px"
+          height="360px"
+          style={{float: 'left'}}
+        />
+        <ReactPlayer
+          url={
+            "https://d3nvdnvywolj6.cloudfront.net/output/capture (9).m3u8"
+          }
+          id="MainPlay"
+          playing={true}
+          loop={true}
+          controls={true}
+          muted={true}
+          width="600px"
+          height="360px"
+          style={{float: 'left'}}
+        />
+        <ReactPlayer
+          url={
+            "https://d3nvdnvywolj6.cloudfront.net/output/capture (10).m3u8"
+          }
+          id="MainPlay"
+          playing={true}
+          loop={true}
+          controls={true}
+          muted={true}
+          width="600px"
+          height="360px"
+          style={{float: 'left'}}
+        />
+        <ReactPlayer
+          url={
+            "https://d3nvdnvywolj6.cloudfront.net/output/capture (11).m3u8"
+          }
+          id="MainPlay"
+          playing={true}
+          loop={true}
+          controls={true}
+          muted={true}
+          width="600px"
+          height="360px"
+          style={{float: 'left'}}
         />
       </Box>
     </>
